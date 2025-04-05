0265bbdd956269c001e7cad584ef75f912119f70
xiaofanwei123 <2633275851@qq.com>
2025-04-05 21:33:40 +0800

    idk

--- src/main/java/com/xiaofanwei/xfws_someitems/data/Language.java
+++ src/main/java/com/xiaofanwei/xfws_someitems/data/Language.java
@@ -1,4 +1,23 @@
 package com.xiaofanwei.xfws_someitems.data;
 
-public class Language {
+import com.xiaofanwei.xfws_someitems.MoreAC;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.LanguageProvider;
+
+public class Language extends LanguageProvider {
+
+    public Language(PackOutput output, String modid, String locale) {
+        super(output, MoreAC.MODID, "en_us");
+    }
+
+    @Override
+    protected void addTranslations() {
+        addTooltips();
+    }
+
+    private void addTooltips() {
+        add("item.xfws_someitems.nature_gift.tooltip", "Nature Gift");
+        add("item.xfws_someitems.mana_flower.tooltip", "Mana Flower");
+        add("item.xfws_someitems.magnet_flower.tooltip", "Magnet Flower");
+    }
 }
